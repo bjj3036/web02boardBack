a49ca5c6443c52c70e5255ed355dbd27613eb610
bjj3036 <baebo0823@naver.com>
2019-06-03 15:53:34 +0900

    20190603 PostController 작성

--- src/main/java/kr/hs/dgsw/board_back/Controller/PostController.java
+++ src/main/java/kr/hs/dgsw/board_back/Controller/PostController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class BoardController {
+public class PostController {
 
     @Autowired
     PostService postService;
